b7b6aab55467e07452a09227ebfdb72daa71b65b
wolpino <ariwolpin@gmail.com>
2026-02-02 12:31:41 -0800

    base site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,20 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Mostly practice!
+          </h1>
+        </div>
+          <Image
           className="dark:invert"
           src="/shelby.jpg"
-          alt="Next.js logo"
+          alt="a dog names shelby"
           width={500}
           height={500}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Step one.
-          </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Next.js typescript app on vercel
               </p>
